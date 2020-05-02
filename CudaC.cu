fdfc9571ae73fbf1c0ced3991515fe6eb79ecf2d
Tdenhof <53848220+Tdenhof@users.noreply.github.com>
2020-05-01 20:05:23 -0400

    Create CudaC.cu

---
+++ CudaC.cu
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define N 10000000
+
+__global__ void c_code(void){
+    
+}
+
+int main(void) {
+c_code<<<1,1>>>();
+int A[10000000], B[10000000], C[10000000];
+int i;
+for (i=0; i<N; i++) {
+C[i] = A[i] + B[i];
+};
+return 0;
+}
